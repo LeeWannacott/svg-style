368eea99373ad8608aad88fd78f5958ebbb0e236
Lee Wannacott <wannacottL@gmail.com>
2021-02-09 00:53:12 +1300

    Added npm folder and published to npm

---
+++ npm/README.md
@@ -0,0 +1,42 @@
+# svg-style-js
+
+Description: Change the style attribute(e.g., fill:color) on svg that are nested in HTML 'img' tags. 
+
+Example:
+
+![Screenshot](rainbowBefore.png)
+
+```html
+<style>
+  .rainbow{
+      display: flex;
+      justify-content: space-around;
+  }
+</style>
+<body>
+    <div class="rainbow">
+        <img class="red" src="macos.svg"><img>
+        <img class="orange" src="macos.svg"><img>
+        <img class="yellow" src="macos.svg"><img>
+        <img class="green" src="macos.svg"><img>
+        <img class="blue" src="macos.svg"><img>
+        <img class="indigo" src="macos.svg"><img>
+        <img class="violet" src="macos.svg"><img>
+
+    </div>
+
+// Loading svg-style
+<script src="svg-style.js"></script>
+// Using svg-style to change the fill property of svg
+<script>
+    svgStyle("red","macos.svg","fill:red;")
+    svgStyle("orange","macos.svg","fill:#FF7F00;")
+    svgStyle("yellow","macos.svg", "fill:yellow;")
+    svgStyle("green","macos.svg","fill:rgb(0, 255, 0);")
+    svgStyle("blue","macos.svg","fill:#FF7F00;")
+    svgStyle("indigo","macos.svg","fill:indigo")
+    svgStyle("violet","macos.svg", "fill:yellow; filter:invert(80%)")
+</script>
+</body>
+```
+![Screenshot](rainbowAfter.png)
---
+++ npm/svg-style.js
@@ -0,0 +1,46 @@
+
+/* 
+svg-style-js
+Author: Lee Wannacott
+Licence: MIT License Copyright (c) 2021 Lee Wannacott 
+GitHub Repository: https://github.com/LeeWannacott/svg-style-js
+
+Instructions: Run this script <script src="svg-style.js"></script>.
+
+Call svgStyle(imageClass,svgFile, style) after script loaded and img tags defined.
+
+imageClass: The class you have put on the img tags.
+svgFile: the svg file that src="" is loading on the img tag.
+style: What you want the style="" to contain e.g. "fill:yellow; filter:invert(80%)"
+
+*/
+
+function svgStyle(imageClass="svg-style",svgFile,style="fill:black"){
+
+    var path = document.location.pathname
+    var directory = path.substring(path.indexOf('/'), path.lastIndexOf('/'))
+    svgFilePath = directory + svgFile
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+
+        if (this.readyState == 4 && this.status == 200) {
+            // Typical action to be performed when the document is ready:
+
+            pathTag = xhttp.responseXML.getElementsByTagName("path")
+
+            for (const styleTag of pathTag){
+                styleTag.setAttribute("style",style);
+            }
+
+            var xmlString = (new XMLSerializer()).serializeToString(xhttp.responseXML); 
+
+            for(let svg of document.getElementsByTagName("img")){
+                if (svg.classList.contains(imageClass)){
+                    svg.outerHTML = xmlString
+                    }
+            }   
+        }
+    };
+    xhttp.open("GET", svgFilePath, true);
+    xhttp.send();
+}
